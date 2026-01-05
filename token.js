1d4e8dc71143d42e0ae3e2a3a50a764850c2c7e3
zenzyx28 <stokzexzxkw@gmail.com>
2026-01-05 12:43:59 +0700

    Clear token.js by developer (manual-trigger)

--- token.js
+++ token.js
@@ -1,7 +1,3 @@
 {
-  "tokens": [
-    "8497425384:AAG6ib9bO2veTxb0-e0bSucJuK_lk3hQ_6A",
-    "8502992464:AAF1YLeeopMpspMXKJXETH1YocQN3iPhmTU",
-    "8578359822:AAE9uRSJfCHswrHXnzc92w5ebavlx_P5ZWM"
-  ]
+  "tokens": []
 }
