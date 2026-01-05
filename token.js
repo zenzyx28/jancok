c8f317c2c0fa4054ab0082342caec57a12cf0e10
zenzyx28 <stokzexzxkw@gmail.com>
2026-01-05 13:26:37 +0700

    add

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8502992464:AAF1YLeeopMpspMXKJXETH1YocQN3iPhmTU"
+    "8502992464:AAF1YLeeopMpspMXKJXETH1YocQN3iPhmTU",
+    "/start 8512658275:AAHUWp2-lqbzXpruri3227YaAe7PHK6Dics"
   ]
 }
