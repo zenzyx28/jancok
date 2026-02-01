186692b7168b6da6916f1187f7bb7af9db3772fd
zenzyx28 <stokzexzxkw@gmail.com>
2026-02-01 09:22:12 +0700

    add token

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8216236411:AAEdo5F7vgaATjW8oTal5lviLjsCcYh5u60",
-    "8427316385:AAEZu0AXontjU-4PeMv17V8lHWyTSpG3Qg4"
+    "8427316385:AAEZu0AXontjU-4PeMv17V8lHWyTSpG3Qg4",
+    "8505747349:AAHaJokO5AOn-yScPpPl1SvXh0o_DmDQZUU"
   ]
 }
